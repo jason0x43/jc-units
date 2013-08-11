4310fbcd42f4022a3d3ba30140e90bfffdac8a52
Jason Cheatham <j.cheatham@gmail.com>
2013-08-11 09:38:40 -0400

    Update workflow to use my alfred lib

--- alfred_convert.py
+++ alfred_convert.py
@@ -1,92 +1,47 @@
 #!/usr/bin/env python
 # coding=UTF-8
 
-from sys import stdout
-import alfred
 import gconvert as convert
 import logging
 import re
-import os.path
+import jalf
 
 
-FORMAT = '%(levelname)s [%(asctime)s] %(name)s - %(message)s'
 LOG = logging.getLogger(__name__)
-LOG_FILE = os.path.join(alfred.get_cache_dir(), 'debug.log')
-
-logging.basicConfig(level=logging.ERROR, format=FORMAT, filename=LOG_FILE)
 
 
-def _out(msg):
-    '''Output a string'''
-    stdout.write(msg.encode('utf-8'))
+class Workflow(jalf.AlfredWorkflow):
+    def _convert(self, value, src_units, dst_units):
+        try:
+            value, text = convert.convert(value, src_units, dst_units)
+        except Exception, e:
+            if e.message.startswith('Parse error in query'):
+                return [jalf.Item('Waiting for input...')]
+            else:
+                try:
+                    int(e.message)
+                    return [jalf.Item('Waiting for input...')]
+                except:
+                    pass
+            raise e
 
+        return [jalf.Item(text, arg=value, valid=True,
+                          subtitle='Action this item to copy {} to the '
+                                   'clipboard'.format(value))]
 
-def tell_convert(query):
-    LOG.debug('called with query "{}"'.format(query))
-    match = re.match('(?P<value>\d+(\.\d+)?)\s*(?P<src>[a-zA-Z]+)(\s+\w+)?'
-                     '\s+(?P<dst>[a-zA-Z]+)', query.strip())
-    if not match:
-        return [alfred.Item('Waiting for input...')]
+    def tell_convert(self, query):
+        LOG.debug('called with query "{}"'.format(query))
 
-    value = float(match.group('value'))
-    src_units = match.group('src').lower()
-    dst_units = match.group('dst').lower()
+        # 1cm [in] ft
+        match = re.match('(?P<value>\d+(\.\d+)?)\s*(?P<src>[a-zA-Z]+)(\s+\w+)?'
+                         '\s+(?P<dst>[a-zA-Z]+)', query.strip())
+        if match:
+            value = float(match.group('value'))
+            src_units = match.group('src').lower()
+            dst_units = match.group('dst').lower()
+            return self._convert(value, src_units, dst_units)
 
-    try:
-        value, text = convert.convert(value, src_units, dst_units)
-    except Exception, e:
-        if e.message.startswith('Parse error in query'):
-            return [alfred.Item('Waiting for input...')]
-        else:
-            try:
-                int(e.message)
-                return [alfred.Item('Waiting for input...')]
-            except:
-                pass
-        raise e
+        return [jalf.Item('Waiting for input...')]
 
-    return [alfred.Item(text, arg=value, valid=True,
-                        subtitle='Action this item to copy {} to the '
-                                 'clipboard'.format(value))]
-
-
-def do_convert(value):
-    _out(value)
-
-
-def tell(name, query=''):
-    '''Tell something'''
-    try:
-        cmd = 'tell_{}'.format(name)
-        if cmd in globals():
-            items = globals()[cmd](query)
-        else:
-            items = [alfred.Item('Invalid action "{}"'.format(name))]
-    except Exception, e:
-        LOG.exception('Exception during tell')
-        items = [alfred.Item('Error: {}'.format(e))]
-
-    _out(alfred.to_xml(items))
-
-
-def do(name, query=''):
-    '''Do something'''
-    try:
-        cmd = 'do_{}'.format(name)
-        if cmd in globals():
-            globals()[cmd](query)
-        else:
-            _out('Invalid command "{}"'.format(name))
-    except Exception, e:
-        LOG.exception('Exception during do')
-        _out('Error: {}'.format(e))
-
-
-if __name__ == '__main__':
-    sh = logging.StreamHandler()
-    sh.setFormatter(logging.Formatter(FORMAT))
-    LOG.addHandler(sh)
-    LOG.setLevel(logging.DEBUG)
-
-    from sys import argv
-    globals()[argv[1]](*argv[2:])
+    def do_convert(self, value):
+        self.puts(value)
